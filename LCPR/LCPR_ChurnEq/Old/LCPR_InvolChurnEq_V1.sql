bd2880b80b2814b2a5e2504ae8d412bdbccc8583
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-05-04 14:08:29 -0500

    Update LCPR_InvolChurnEq_V1.sql

--- LCPR/LCPR_ChurnEq/Old/LCPR_InvolChurnEq_V1.sql
+++ LCPR/LCPR_ChurnEq/Old/LCPR_InvolChurnEq_V1.sql
@@ -43,7 +43,42 @@ LEFT JOIN repeated_accounts R
 --- --- --- --- --- --- --- --- --- --- --- --- ---  DNA --- --- --- --- --- --- --- --- --- --- --- ---
 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
+-- ,Invol_Funnel_Fields AS(
+-- select  
+--     *,
+--     first_value(LagDueDay_feb) over(partition by act_acct_cd, DATE(DATE_TRUNC('MONTH',dt)) order by date(dt) desc) as LastDueDay_feb
+-- from(
+--     SELECT 
+--         DISTINCT DATE(DATE_TRUNC('MONTH',date(d.dt))) AS Month,
+--         date(d.dt) AS dt,
+--         DATE(DATE_TRUNC('MONTH',fi_bill_dt_m0)) AS BillMonth,
+        -- date(fi_bill_dt_m0) as BillDay,
+        -- d.act_acct_cd,
+        -- d.fi_outst_age AS DueDays,
+--         CASE WHEN ACT_BLNG_CYCL IN('A','B','C') THEN 15 ELSE 28 END AS FirstOverdueDay,
+--         case when DATE(DATE_TRUNC('MONTH',date(d.dt)))=date('2022-03-01') then date('2022-03-02') else DATE(DATE_TRUNC('MONTH',date(d.dt))) end as Backlog_Date,
+--         first_value(fi_outst_age) over(partition by act_acct_cd,DATE(DATE_TRUNC('MONTH',date(d.dt))) order by date(dt) desc) as LastDueDay,oldest_unpaid_bill_dt,
+--         lag(fi_outst_age) over(partition by act_acct_cd order by date(dt) asc) as LagDueDay_feb
+--     FROM "db-analytics-prod"."fixed_cwp" d
+--     WHERE act_cust_typ_nm = 'Residencial' and date(dt) between (select start_date from parameters ) and (select end_date from parameters )
+--     )
+-- )
 
+, funnel1_dna as (
+SELECT
+    date_trunc('montn', date(dt)) as month, 
+    date(dt) as dt, 
+    date_trunc('month', date(bill_from_dte_sbb)) as billmonth,
+    date(bill_from_dte_sbb) as billday,
+    sub_acct_no_sbb, 
+    delinquency_days as duedays, 
+    
+)
+
+SELECT
+    *
+FROM "lcpr.stage.prod"."insights_customer_services_rates_lcpr" 
+LIMIT 100
 
 --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 --- --- --- --- --- --- --- --- --- --- --- ---  Overdueday1 --- --- --- --- --- --- --- --- --- --- ---
