bc2e7062ea0f94e933d3da77fa51a42b9fb9e391
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-05-31 15:27:41 -0500

    Update LCPR_Mobile_NewLogics_Disco&Susp_RecoveringFakeChurns.sql

--- LCPR/LCPR_Mobile/LCPR_Mobile_NewLogics_Disco&Susp_RecoveringFakeChurns.sql
+++ LCPR/LCPR_Mobile/LCPR_Mobile_NewLogics_Disco&Susp_RecoveringFakeChurns.sql
@@ -11,28 +11,92 @@ SELECT  DATE_TRUNC('month',DATE('2023-04-01')) AS input_month
         ,85 as overdue_days
 )
 
-, cust_mstr_adj as (
+, cust_mstr_adj_pre as (
 SELECT
     *, 
-    first_value(date(subsrptn_actvtn_dt)) over (partition by subsrptn_id order by dt asc) as activation_dt
+    first_value(date(subsrptn_actvtn_dt)) over (partition by subsrptn_id order by dt asc) as activation_dt, 
+    1 as row_nm
 FROM "lcpr.stage.dev"."tbl_pstpd_cust_mstr_ss_data"
-WHERE date(dt) between ((SELECT input_month FROM parameters) - interval '3' month) and (SELECT input_month FROM parameters)
+WHERE date(dt) between ((SELECT input_month FROM parameters) - interval '6' month) and (SELECT input_month FROM parameters)
+)
+
+, fake_churns_pre as (
+SELECT
+    DATE_TRUNC('MONTH',DATE(dt)) AS month,
+    subsrptn_id AS fake_churn_id, 
+    acct_sts_rsn_desc, 
+    case when 
+        lower(acct_sts_rsn_desc) like '%contract%accepted%'
+        or lower(acct_sts_rsn_desc) like '%portin%'
+        or lower(acct_sts_rsn_desc) like '%ctn%activation%'
+        or lower(acct_sts_rsn_desc) like '%per%cust%req%'
+        or lower(acct_sts_rsn_desc) like '%reduced%rate%suspend%'
+        or lower(acct_sts_rsn_desc) like '%""%'
+        or lower(lst_susp_rsn_desc) like '%""%'
+    then subsrptn_id else null end as invalid_reasons_flag, 
+    first_value(date(subsrptn_sts_dt)) over (partition by subsrptn_id order by dt desc) as last_disco_dt, 
+    row_number() over (partition by subsrptn_id order by date(subsrptn_sts_dt) desc) as row_nm
+FROM "lcpr.stage.dev"."tbl_pstpd_cust_cxl_incr_data"
+WHERE 
+    date_trunc('month', date(dt)) between ((SELECT input_month FROM parameters) - interval '6' month) and ((SELECT input_month FROM parameters) - interval '1' month)
+)
+
+, fake_churns as (
+SELECT
+    month, 
+    fake_churn_id,
+    acct_sts_rsn_desc, 
+    invalid_reasons_flag, 
+    last_disco_dt
+FROM fake_churns_pre
+WHERE
+    row_nm = 1
+    and 
+    invalid_reasons_flag is not null
+)
+
+, missing_accounts as (
+SELECT
+    *, 
+    first_value(date(subsrptn_actvtn_dt)) over (partition by subsrptn_id order by dt asc) as activation_dt,
+    row_number() over (partition by subsrptn_id order by dt desc) as row_nm, 
+    (SELECT input_month FROM parameters) as dt_2
+FROM "lcpr.stage.dev"."tbl_pstpd_cust_mstr_ss_data"
+WHERE 
+    date(dt) between ((SELECT input_month FROM parameters) - interval '6' month) and ((SELECT input_month FROM parameters) - interval '1' month)
+    and subsrptn_id in (SELECT fake_churn_id FROM fake_churns)
+)
+
+, cust_mstr_adj as (
+SELECT
+    *
+FROM missing_accounts
+WHERE row_nm = 1
+
+UNION ALL
+
+SELECT
+    *, 
+    date(dt) as dt_2
+FROM cust_mstr_adj_pre
+WHERE
+    subsrptn_id not in (SELECT fake_churn_id FROM fake_churns)
 )
 
 ,BOM_active_base AS (
 SELECT  subsrptn_id AS account
         ,cust_id AS parent_account
-        ,(date(dt) + interval '1' month - interval '1' day) AS mob_b_dim_date
-        ,DATE_DIFF('day',date(activation_dt),(date(dt) + interval '1' month - interval '1' day)) AS mob_b_mes_TenureDays
+        ,(date(dt_2) + interval '1' month - interval '1' day) AS mob_b_dim_date
+        ,DATE_DIFF('day',date(activation_dt),(date(dt_2) + interval '1' month - interval '1' day)) AS mob_b_mes_TenureDays
         ,date(activation_dt) AS mob_b_att_MaxStart
-        ,CASE   WHEN DATE_DIFF('day',date(activation_dt),(date(dt) + interval '1' month - interval '1' day)) <= 180 THEN 'Early-Tenure'
-                WHEN DATE_DIFF('day',date(activation_dt),(date(dt) + interval '1' month - interval '1' day)) <= 360 THEN 'Mid-Tenure'        
-                WHEN DATE_DIFF('day',date(activation_dt),(date(dt) + interval '1' month - interval '1' day)) >  360 THEN 'Late-Tenure'
+        ,CASE   WHEN DATE_DIFF('day',date(activation_dt),(date(dt_2) + interval '1' month - interval '1' day)) <= 180 THEN 'Early-Tenure'
+                WHEN DATE_DIFF('day',date(activation_dt),(date(dt_2) + interval '1' month - interval '1' day)) <= 360 THEN 'Mid-Tenure'        
+                WHEN DATE_DIFF('day',date(activation_dt),(date(dt_2) + interval '1' month - interval '1' day)) >  360 THEN 'Late-Tenure'
                     ELSE NULL END AS mob_b_fla_Tenure
         ,null AS mob_b_mes_MRC
         ,1 AS mob_b_mes_numRGUS
 FROM cust_mstr_adj
-WHERE date(dt) = (SELECT input_month FROM parameters) - interval '1' month
+WHERE date(dt_2) = (SELECT input_month FROM parameters) - interval '1' month
     --> Flags utilizadas para clasificar a los usuarios residenciales. Input de Juan C. Vega
     AND cust_sts = 'O'
     AND acct_type_cd = 'I'
@@ -43,17 +107,17 @@ WHERE date(dt) = (SELECT input_month FROM parameters) - interval '1' month
 ,EOM_active_base AS (
 SELECT  subsrptn_id as account
         ,cust_id AS parent_account
-        ,(date(dt) + interval '1' month - interval '1' day) AS mob_e_dim_date
-        ,DATE_DIFF('day',date(activation_dt),(date(dt) + interval '1' month - interval '1' day)) AS mob_e_mes_TenureDays
+        ,(date(dt_2) + interval '1' month - interval '1' day) AS mob_e_dim_date
+        ,DATE_DIFF('day',date(activation_dt),(date(dt_2) + interval '1' month - interval '1' day)) AS mob_e_mes_TenureDays
         ,date(activation_dt) AS mob_e_att_MaxStart
-        ,CASE   WHEN DATE_DIFF('day',date(activation_dt),(date(dt) + interval '1' month - interval '1' day)) <= 180 THEN 'Early-Tenure'
-                WHEN DATE_DIFF('day',date(activation_dt),(date(dt) + interval '1' month - interval '1' day)) <= 360 THEN 'Mid-Tenure'        
-                WHEN DATE_DIFF('day',date(activation_dt),(date(dt) + interval '1' month - interval '1' day)) >  360 THEN 'Late-Tenure'
+        ,CASE   WHEN DATE_DIFF('day',date(activation_dt),(date(dt_2) + interval '1' month - interval '1' day)) <= 180 THEN 'Early-Tenure'
+                WHEN DATE_DIFF('day',date(activation_dt),(date(dt_2) + interval '1' month - interval '1' day)) <= 360 THEN 'Mid-Tenure'        
+                WHEN DATE_DIFF('day',date(activation_dt),(date(dt_2) + interval '1' month - interval '1' day)) >  360 THEN 'Late-Tenure'
                     ELSE NULL END AS mob_e_fla_Tenure
         ,null AS mob_e_mes_MRC
         ,1 AS mob_e_mes_numRGUS
 FROM cust_mstr_adj
-WHERE date(dt) = (SELECT input_month FROM parameters)
+WHERE date(dt_2) = (SELECT input_month FROM parameters)
         --> Flags utilizadas para clasificar a los usuarios residenciales. Input de Juan C. Vega
         AND cust_sts = 'O'
         AND acct_type_cd = 'I'
@@ -70,7 +134,10 @@ SELECT  (SELECT input_month FROM parameters) AS mob_s_dim_month
                 WHEN (A.parent_account IS NULL AND B.parent_account IS NOT NULL) THEN B.parent_account
                     ELSE NULL END AS mob_s_att_ParentAccount
         ,'Postpaid' AS mob_s_att_MobileType
-        ,IF(A.account IS NOT NULL,1,0) AS mob_b_att_active
+        ,case 
+            when B.account in (SELECT subsrptn_id FROM missing_accounts) then 1
+            when A.account is not null then 1 
+        else 0 end AS mob_b_att_active
         ,IF(B.account IS NOT NULL,1,0) AS mob_e_att_active
         ,mob_b_dim_date
         ,mob_b_mes_TenureDays
@@ -155,38 +222,16 @@ WHERE
         )
 )
 
-, fake_churns as (
-SELECT
-    date_trunc('month', date(dt)) as month,
-    subsrptn_id as fake_churn_id
-FROM "lcpr.stage.dev"."tbl_pstpd_cust_cxl_incr_data"
-WHERE 
-    DATE(dt) between ((SELECT input_month FROM parameters) - interval '6' month) and ((SELECT input_month FROM parameters) - interval '1' month)
-    --AND cust_sts = 'O'
-    AND acct_type_cd = 'I'
-    AND rgn_nm <> 'VI'
-        --AND subsrptn_sts <> 'A'
-    AND subsrptn_id NOT IN (SELECT DISTINCT subsrptn_id
-                        FROM "lcpr.stage.dev"."tbl_pstpd_cust_cxl_incr_data"
-                        WHERE DATE(dt) = (SELECT input_month FROM parameters) - interval '1' month
-                            --AND cust_sts = 'O'
-                            AND acct_type_cd = 'I'
-                            AND rgn_nm <> 'VI'
-                            --AND subsrptn_sts = 'A'
-                        )
-    AND lower(acct_sts_rsn_desc) not like '%contract%accept%'
-    AND lower(lst_susp_rsn_desc) not like '%""%'
-)
 
 ,main_movement_flag AS(
 SELECT  *
-        ,CASE   WHEN (mob_e_mes_numRGUS - mob_b_mes_numRGUS) = 0 THEN '1.SameRGUs' 
+        ,CASE   WHEN (mob_e_mes_numRGUS - mob_b_mes_numRGUS) = 0 or mob_s_att_account in (SELECT fake_churn_id FROM fake_churns) THEN '1.SameRGUs' 
                 WHEN (mob_e_mes_numRGUS - mob_b_mes_numRGUS) > 0 THEN '2.Upsell'
                 WHEN (mob_e_mes_numRGUS - mob_b_mes_numRGUS) < 0 THEN '3.Downsell'
                 -- WHEN (mob_b_mes_numRGUS IS NULL AND mob_e_mes_numRGUS > 0 AND DATE_TRUNC('MONTH',mob_e_att_MaxStart) =  mob_s_dim_month) THEN '4.New Customer'
                 -- WHEN (mob_b_mes_numRGUS IS NULL AND mob_e_mes_numRGUS > 0 AND DATE_TRUNC('MONTH',mob_e_att_MaxStart) <> mob_s_dim_month) THEN '5.Come Back to Life'
-                WHEN (mob_b_mes_numRGUS IS NULL AND mob_e_mes_numRGUS > 0 AND newcust_candidate_flag is not null AND A.mob_s_att_account not in (SELECT fake_churn_id FROM fake_churns)) THEN '4.New Customer'
-                WHEN (mob_b_mes_numRGUS IS NULL AND mob_e_mes_numRGUS > 0 AND newcust_candidate_flag is null AND A.mob_s_att_account not in (SELECT fake_churn_id FROM fake_churns)) THEN '5.Come Back to Life'
+                WHEN (mob_b_mes_numRGUS IS NULL AND mob_e_mes_numRGUS > 0 AND newcust_candidate_flag is not null) and mob_s_att_account not in (SELECT fake_churn_id FROM fake_churns) THEN '4.New Customer'
+                WHEN (mob_b_mes_numRGUS IS NULL AND mob_e_mes_numRGUS > 0 AND newcust_candidate_flag is null) and mob_s_att_account not in (SELECT fake_churn_id FROM fake_churns) THEN '5.Come Back to Life'
                 WHEN (mob_b_mes_numRGUS > 0 AND mob_e_mes_numRGUS IS NULL) THEN '6.Null last day'
                 WHEN (mob_b_mes_numRGUS IS NULL AND mob_e_mes_numRGUS IS NULL) THEN '7.Always null'
                     END AS mob_s_fla_MainMovement
@@ -231,19 +276,24 @@ SELECT  DATE_TRUNC('MONTH',DATE(dt)) AS month
 FROM "lcpr.stage.dev"."tbl_pstpd_cust_cxl_incr_data"
 WHERE DATE(dt) = (SELECT input_month FROM parameters)
         --AND cust_sts = 'O'
-        AND acct_type_cd = 'I'
-        AND rgn_nm <> 'VI'
+    AND acct_type_cd = 'I'
+    AND rgn_nm <> 'VI'
         --AND subsrptn_sts <> 'A'
-    AND subsrptn_id NOT IN (SELECT DISTINCT subsrptn_id
-                        FROM "lcpr.stage.dev"."tbl_pstpd_cust_cxl_incr_data"
-                        WHERE DATE(dt) = (SELECT input_month FROM parameters) - interval '1' month
-                            --AND cust_sts = 'O'
-                            AND acct_type_cd = 'I'
-                            AND rgn_nm <> 'VI'
-                            --AND subsrptn_sts = 'A'
-                        )
-        AND lower(acct_sts_rsn_desc) not like '%contract%accept%'
-        AND lower(lst_susp_rsn_desc) not like '%""%'
+    -- AND subsrptn_id NOT IN (SELECT DISTINCT subsrptn_id
+    --                     FROM "lcpr.stage.dev"."tbl_pstpd_cust_cxl_incr_data"
+    --                     WHERE DATE(dt) = (SELECT input_month FROM parameters) - interval '1' month
+    --                         --AND cust_sts = 'O'
+    --                         AND acct_type_cd = 'I'
+    --                         AND rgn_nm <> 'VI'
+    --                         --AND subsrptn_sts = 'A'
+    --                     )
+    and lower(acct_sts_rsn_desc) not like '%contract%accepted%'
+    and lower(acct_sts_rsn_desc) not like '%portin%'
+    and lower(acct_sts_rsn_desc) not like '%ctn%activation%'
+    and lower(acct_sts_rsn_desc) not like '%per%cust%req%'
+    and lower(acct_sts_rsn_desc) not like '%reduced%rate%suspend%'
+    and lower(acct_sts_rsn_desc) not like '%""%'
+    and lower(lst_susp_rsn_desc) not like '%""%'
 )
 
 ,all_churners AS (
@@ -333,21 +383,15 @@ WHERE mob_e_mes_TenureDays < 0
     
 )
 
--- SELECT 
---     A.mob_s_att_account, 
---     B.acct_sts_rsn_desc, 
---     B.lst_susp_rsn_desc
--- FROM full_flags A
--- LEFT JOIN "lcpr.stage.dev"."tbl_pstpd_cust_cxl_incr_data" B
---     ON A.mob_s_att_account = B.subsrptn_id and date(A.mob_s_dim_month) = date_trunc('month', date(B.dt)) 
+-- SELECT *
+-- FROM full_flags
 -- WHERE 
---     A.mob_b_att_active + A.mob_e_att_active >= 1
---     and cast(A.mob_s_att_account as varchar) in ('7872200680', '7872355566', '7873084424', '7873086985', '7873601540', '7874003884', '7874367378', '7874772267', '7875857903', '7875901235', '7875907746', '7875907949', '7876003125', '7876004427', '7876004434', '7876640441', '7876648013', '7877182218', '7879081125', '7879082706', '7879087088')
---     and DATE(B.dt) = (SELECT input_month FROM parameters)
---         --AND cust_sts = 'O'
---         AND B.acct_type_cd = 'I'
---         AND B.rgn_nm <> 'VI'
-        --AND subsrptn_sts <> 'A'
+--     mob_b_att_active + mob_e_att_active >= 1
+--     -- and mob_s_att_account = 7876004427
+
+-- SELECT
+--     *
+-- FROM fake_churns
 
 SELECT
     mob_s_dim_month, 
@@ -366,9 +410,3 @@ WHERE
     -- and mob_s_att_account not in (SELECT mob_s_att_account FROM extra_users)
 GROUP BY 1, 2, 3, 4, 5, 6, 7
 ORDER BY 4 asc, 5 asc, 6 asc, 7 asc
-
--- SELECT
---     *
--- FROM full_flags
--- WHERE
---     mob_b_att_active + mob_e_att_active >= 1
