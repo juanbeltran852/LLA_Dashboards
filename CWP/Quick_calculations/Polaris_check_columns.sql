d987be5abb48be522920c90f9eaef01bff76255a
juanbeltran852 <125683314+juanbeltran852@users.noreply.github.com>
2023-06-20 11:53:33 -0500

    Create Polaris_check_columns.sql

---
+++ CWP/Quick_calculations/Polaris_check_columns.sql
@@ -0,0 +1,58 @@
+SELECT
+    -- distinct billableaccountno
+    -- distinct billablename
+    -- distinct vip
+    -- distinct cedula, count(*) as num_registros
+    -- distinct phone, count(*) as num_registros
+    -- distinct serviceno, count(*) as num_registos
+    -- distinct email, count(*) as num_registos
+    -- distinct address, count(*) as num_registos
+    -- distinct cycle, count(*) as num_registros
+    -- distinct category, count(*) as num_registos
+    -- distinct metodo_pago, count(*) as num_registos
+    -- distinct stopdate, count(*) as num_registos
+    -- distinct dias_de_atraso, count(*) as num_registos
+    -- distinct currentdays, count(*) as num_registos
+    -- distinct de_0_a_30_dias, count(*) as num_registos
+    -- distinct de_31_a_60_dias, count(*) as num_registos 
+    -- distinct de_61_a_90_dias, count(*) as num_registos 
+    -- distinct de_91_a_120_dias, count(*) as num_registos 
+    -- distinct de_121_a_150_dias, count(*) as num_registos 
+    -- distinct de_151_a_180_dias, count(*) as num_registos 
+    -- distinct de_181_a_210_dias, count(*) as num_registos 
+    -- distinct de_211_a_240_dias, count(*) as num_registos 
+    -- distinct de_241_a_270_dias, count(*) as num_registos 
+    -- distinct de_271_a_300_dias, count(*) as num_registos 
+    -- distinct de_301_a_330_dias, count(*) as num_registos 
+    -- distinct de_331_a_360_dias, count(*) as num_registos 
+    -- distinct dias_730, count(*) as num_registos 
+    -- distinct dias_1095, count(*) as num_registos 
+    -- distinct dias_1460, count(*) as num_registos 
+    -- distinct dias_1825, count(*) as num_registos 
+    -- distinct dias_2000, count(*) as num_registos 
+    -- distinct total
+    -- distinct monto_vencido
+    -- distinct advancepay, count(*) as num_registos 
+    -- distinct razon_desconexion, count(*) as num_registos 
+    -- distinct customerno, count(*) as num_registos 
+    -- distinct account_status, count(*) as num_registos 
+    -- distinct cant_facturas, count(*) as num_registos 
+    -- distinct canal_activacion, count(*) as num_registos 
+    -- distinct vendedor, count(*) as num_registos 
+    -- distinct tiendas, count(*) as num_registos 
+    -- distinct plan_especial_moratoria, count(*) as num_registos 
+    -- distinct status_bloqueo, count(*) as num_registos 
+    -- distinct fecha_inicio_factura, count(*) as num_registos 
+    -- distinct fecha_vencimiento_factura, count(*) as num_registos 
+    -- distinct dt
+    
+    
+    
+FROM "test_ap"."polaris_new_polaris_campaigns2"
+
+-- GROUP BY 1
+
+-- ORDER BY 1 desc
+-- ORDER BY 1 asc
+
+-- LIMIT 10
